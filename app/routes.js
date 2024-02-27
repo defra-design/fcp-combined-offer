1702e472e743f41927cf41e4d5d7ae13ebbb1720
Vi Tan <virginie.tan@capgemini.com>
2024-02-27 14:34:45 +0000

    Update with E2E

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,105 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+router.post('/land-details-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var landDetailsAnswer = req.session.data['land-details-answer']
+
+  // Check whether the variable matches a condition
+  if (landDetailsAnswer == "yes"){
+    // Send user to next page
+    res.redirect('/e2e-mvp/tasklist-2')
+
+  } else {
+    // Send user to ineligible page
+    res.redirect('/e2e-mvp/update-land-details')
+  }
+
+})
+
+router.post('/management-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var managementControlAnswer = req.session.data['management-answer']
+
+  // Check whether the variable matches a condition
+  if (managementControlAnswer == "yes"){
+    // Send user to next page
+    res.redirect('/e2e-mvp/hefer')
+
+  } else {
+    // Send user to ineligible page
+    res.redirect('/e2e-mvp/ineligible')
+  }
+
+})
+
+router.post('/hefer-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var managementControlAnswer = req.session.data['hefer-answer']
+
+  // Check whether the variable matches a condition
+  if (managementControlAnswer == "yes"){
+    // Send user to next page
+    res.redirect('/e2e-mvp/sssi')
+
+  } else {
+    // Send user to ineligible page
+    res.redirect('/e2e-mvp/ineligible')
+  }
+
+})
+
+router.post('/sssi-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var managementControlAnswer = req.session.data['sssi-answer']
+
+  // Check whether the variable matches a condition
+  if (managementControlAnswer == "yes"){
+    // Send user to next page
+    res.redirect('/e2e-mvp/public-body')
+
+  } else {
+    // Send user to ineligible page
+    res.redirect('/e2e-mvp/ineligible')
+  }
+
+})
+
+router.post('/public-body-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var managementControlAnswer = req.session.data['public-body-answer']
+
+  // Check whether the variable matches a condition
+  if (managementControlAnswer == "yes"){
+    // Send user to next page
+    res.redirect('/e2e-mvp/ite')
+
+  } else {
+    // Send user to ineligible page
+    res.redirect('/e2e-mvp/ineligible')
+  }
+
+})
+
+router.post('/ite-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var managementControlAnswer = req.session.data['ite-answer']
+
+  // Check whether the variable matches a condition
+  if (managementControlAnswer == "yes"){
+    // Send user to next page
+    res.redirect('/e2e-mvp/eligible')
+
+  } else {
+    // Send user to ineligible page
+    res.redirect('/e2e-mvp/ineligible')
+  }
+
+})
+
